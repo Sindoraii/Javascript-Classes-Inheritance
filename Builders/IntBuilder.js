69fabe8d2e914dbe39d7a9b7e26a488174f6fdf5
Mariya Krupenina <marise.sindorai@yandex.ru>
2022-11-30 16:42:13 +0300

    feat: create children of BaseBulder and show result of method chaining

---
+++ Builders/IntBuilder.js
@@ -0,0 +1,17 @@
+import BaseBuilder from "./BaseBuilder.js";
+
+class IntBuilder extends BaseBuilder {
+    constructor(init = 0) {
+        super(init);
+    }
+    static random(from, to) {
+        from = Math.trunc(from);
+        to = Math.trunc(to);
+        return Math.floor(Math.random() * (to - from +1 )) + from;
+    }
+    mod(num){
+        this.init = this.init % num;
+        return this;
+    }
+}
+export default IntBuilder;
